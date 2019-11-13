14baf3e8696f667c21e25a0f6c049a160cfa4c15
HarshaTY-Demo <harsha.k@testyantra.com>
2019-11-13 12:19:02 +0530

    updated

--- src/com/ty/prema/DemoA.java
+++ src/com/ty/prema/DemoA.java
@@ -4,7 +4,7 @@ public class DemoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("remote");
+     System.out.println("remote area");
 	}
 
 }
